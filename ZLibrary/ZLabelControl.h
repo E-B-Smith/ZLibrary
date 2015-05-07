1404dea0f244da6e3127809c370f39427bc6547c
Edward B. Smith <smith.ed.b@gmail.com>
2015-05-07 03:52:37 -0700

    .

---
+++ ZLibrary/ZLabelControl.h
@@ -0,0 +1,15 @@
+//
+//  ZLabelControl.h
+//  Happiness
+//
+//  Created by Edward Smith on 5/6/15.
+//  Copyright (c) 2015 Edward Smith. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ZLabelControl : UIControl
+@property (strong, nonatomic) IBOutlet UILabel *label;
+@end
---
+++ ZLibrary/ZLabelControl.m
@@ -0,0 +1,54 @@
+//
+//  ZLabelControl.m
+//  Happiness
+//
+//  Created by Edward Smith on 5/6/15.
+//  Copyright (c) 2015 Edward Smith. All rights reserved.
+//
+
+
+#import "ZLabelControl.h"
+
+
+@implementation ZLabelControl
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder
+	{
+	self = [super initWithCoder:aDecoder];
+	[self commonInit];
+	return self;
+	}
+
+- (instancetype) initWithFrame:(CGRect)frame
+	{
+	self = [super initWithFrame:frame];
+	[self commonInit];
+	return self;
+	}
+
+- (void) commonInit
+	{
+	self.userInteractionEnabled = YES;
+	}
+
+- (UILabel*) label
+	{
+	if (!_label)
+		{
+		_label = [[UILabel alloc] initWithFrame:self.bounds];
+		[self addSubview:_label];
+		}
+	return _label;
+	}
+
+- (void) layoutSubviews
+	{
+	if (_label)
+		{
+		_label.frame = self.bounds;
+		_label.userInteractionEnabled = NO;
+		[self addSubview:_label];
+		}
+	}
+
+@end
